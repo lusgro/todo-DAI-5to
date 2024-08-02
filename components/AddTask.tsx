f4cb055b23621296de2abf415980d48fd934b65e
Rameik <ramakwon07@gmail.com>
2024-08-02 10:37:02 -0300

    .

--- components/AddTask.tsx
+++ components/AddTask.tsx
@@ -1,5 +1,5 @@
 import { Input, Stack } from "native-base";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { Button } from "native-base";
 import { useTasks } from "@/hooks/useTasks";
 import { useState } from "react";
@@ -10,13 +10,14 @@ export default function AddTask() {
     const { addToList } = useTasks()
 
     const handleSubmit = () => {
+        console.log(newTask)
         if(newTask != "") addToList(newTask)
     }
 
     return (
-        <View>
-            <Stack space={1} w="75%" maxW="300px" mx="auto">
-                <Input size="sm" placeholder="sm Input" onChangeText={setNewTask} value={newTask}/>
+        <View style={styles.addTask}>
+            <Stack space={1} w="40%" maxW="200px" mx="auto">
+                <Input size="sm" placeholder="Agregar tarea" onChangeText={setNewTask} value={newTask}/>
             </Stack>;
             <Button onPress={handleSubmit} colorScheme="success">
                 Crear
@@ -24,3 +25,11 @@ export default function AddTask() {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    addTask: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        gap: 20
+    }
+})
